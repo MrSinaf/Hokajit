a0877dc8820de0e4046bca20f13222845e9aa1de
MrSinaf <mrsinaf@sinafproduction.xyz>
2026-05-27 16:14:58 +0200

    (๑•̀ㅂ•́)و Update `ContextMenu`

--- Hokajit.Game/UI/ContextMenu.cs
+++ Hokajit.Game/UI/ContextMenu.cs
@@ -23,9 +23,10 @@ public sealed class ContextMenu : UIElement
 	
 	public ContextMenu()
 	{
+		isInteractive = false;
+		active = false;
 		var ui = Vault.GetAsset<Texture2D>("ui");
 		mesh = Vault.GetAsset<Mesh>(UIModule.DEFAULT_MESH);
-		// material = new MaterialUI().SetTexture(ui).SetNinePatch(new Region(2), 3);
 		uv = ui.GetUVRegion(new RectInt(8, 8, 6, 4));
 		size = new Vector2(200, 32);
 		pivot = new Vector2(0.5F, 0);
